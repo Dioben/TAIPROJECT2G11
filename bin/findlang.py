dea188b00be092b28cf939c547327b7b384d9a84
buckaroo69 <diogobento@ua.pt>
2021-12-02 11:11:27 +0000

    remove .order dependency

--- bin/findlang.py
+++ bin/findlang.py
@@ -18,12 +18,14 @@ if __name__ == "__main__":
 
     bestcost = math.inf
     bestmodel = None
+
     for f in os.listdir(args.classes):
         keyname = f.removesuffix(".tar.gz")
         fullpath = f"{args.classes}/{f}"
         fileobj = gzip.open(fullpath,"rt")
         model = json.load(fileobj)
-        start_up = sorted(model['alphabet'])[0]*args.order
+        order = len( list(model['model'].keys())[0] )#get length of a random key to know order, all the keys in a given model have same order anyway
+        start_up = sorted(model['alphabet'])[0]*order
         default_cost = -math.log2(1/len(model['alphabet'])) #in case we haven't seen a prefix
         filesize = common_modules.calculateFileSize(model['model'],text,start_up,default_cost)
         if filesize<bestcost:
