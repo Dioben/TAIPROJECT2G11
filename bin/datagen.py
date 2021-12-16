64f1847a7936238b3da6b7bbb6a44485f960de67
p-laranjinha <plcasimiro@ua.pt>
2021-12-16 19:18:19 +0000

    datagen progress + added % to model compiler

--- bin/datagen.py
+++ bin/datagen.py
@@ -4,6 +4,7 @@ import os
 import common_modules
 import gzip
 import json
+from model_compiler import main as model_compiler
 
 
 def findsize(text, model):
@@ -14,47 +15,71 @@ def findsize(text, model):
     filesize = common_modules.calculateFileSize(model,text,start_up,default_cost,notInModelCost)
     return filesize
 
+
+def getTextCosts(costs, textDir, model):
+    for textName in os.listdir(textDir):
+        with open(f"{textDir}/{textName}", "r") as file:
+            cost = findsize(file.read(), model)
+            keyTextName = textName.removesuffix("-test.utf8")
+            if keyTextName not in costs:
+                costs[keyTextName] = {}
+            costs[keyTextName][keyModelName] = cost
+    return costs
+
+
+def getLineCosts(costs, textDir, model):
+    for textName in os.listdir(textDir):
+        with open(f"{textDir}/{textName}", "r") as file:
+            i = -1
+            for line in file.readlines():
+                i+=1
+                line = line.split("	", 1)[1]
+                cost = findsize(line, model)
+                keyTextName = textName.removesuffix("-test.utf8")
+                if keyTextName not in costs:
+                    costs[keyTextName] = {}
+                if i not in costs[keyTextName]:
+                    costs[keyTextName][i] = {}
+                costs[keyTextName][i][keyModelName] = cost
+    return costs
+
+
 if __name__ == "__main__":
     parser= argparse.ArgumentParser()
-    parser.add_argument("--classes",help="Class models source folder", required=True)
+    parser.add_argument("--folder", help="Models folder", required=True)
     # parser.add_argument("--fulltest-dir",help="Folder with full texts under analysis", required=True)
+    # parser.add_argument("--longtest-dir",help="Folder with long texts under analysis", required=True)
+    # parser.add_argument("--mediumtest-dir",help="Folder with medium texts under analysis", required=True)
     # parser.add_argument("--shorttest-dir",help="Folder with short texts under analysis", required=True)
+    # parser.add_argument("--tinytest-dir",help="Folder with tiny texts under analysis", required=True)
     args = parser.parse_args()
 
     args.fulltest_dir = "../DS/test/"
+    args.longtest_dir = "../DS/testsets-long/"
+    args.mediumtest_dir = "../DS/testsets-medium/"
     args.shorttest_dir = "../DS/testsets-short/"
+    args.tinytest_dir = "../DS/testsets-tiny/"
 
-    fullTestCosts = {}
-    shortTestCosts = {}
+    accuracies = {}
 
-    for f in os.listdir(args.classes):
-        keyModelName = f.removesuffix(".tar.gz").removesuffix("-train")
-        fullpath = f"{args.classes}/{f}"
-        fileobj = gzip.open(fullpath,"rt")
-        model = json.load(fileobj)
+    for modelSize in [i/10 for i in range(1, 11)]:
 
-        for textName in os.listdir(args.fulltest_dir):
-            with open(f"{args.fulltest_dir}/{textName}", "r") as file:
-                cost = findsize(file.read(), model)
-                keyTextName = textName.removesuffix("-test.utf8")
-                if keyTextName not in fullTestCosts:
-                    fullTestCosts[keyTextName] = {}
-                fullTestCosts[keyTextName][keyModelName] = cost
-    
-        for textName in os.listdir(args.shorttest_dir):
-            with open(f"{args.shorttest_dir}/{textName}", "r") as file:
-                i = -1
-                for line in file.readlines():
-                    i+=1
-                    line = line.split("	", 1)[1]
-                    cost = findsize(line, model)
-                    keyTextName = textName.removesuffix("-test.utf8")
-                    if keyTextName not in shortTestCosts:
-                        shortTestCosts[keyTextName] = {}
-                    if i not in shortTestCosts[keyTextName]:
-                        shortTestCosts[keyTextName][i] = {}
-                    shortTestCosts[keyTextName][i][keyModelName] = cost
+        modelDir = f'testModels{int(modelSize*10)}/'
+        if not os.path.isdir(modelDir):
+            model_compiler(3, 0.1, args.folder, modelDir, modelSize)
 
-    # compare text and model name to know if correct
-    for textName, models in fullTestCosts.items():
-        print(f'{textName}: {models}')
+        costs = {"full":{}, "long":{}, "medium":{}, "short":{}, "tiny":{}}
+
+        for f in os.listdir(modelDir):
+            keyModelName = f.removesuffix(".tar.gz").removesuffix("-train")
+            fullpath = f"{modelDir}{f}"
+            fileobj = gzip.open(fullpath,"rt")
+            model = json.load(fileobj)
+
+            costs["full"] = getTextCosts(costs["full"], args.fulltest_dir, model)
+            costs["long"] = getLineCosts(costs["long"], args.longtest_dir, model)
+            costs["medium"] = getLineCosts(costs["medium"], args.mediumtest_dir, model)
+            costs["short"] = getLineCosts(costs["short"], args.shorttest_dir, model)
+            costs["tiny"] = getLineCosts(costs["tiny"], args.tinytest_dir, model)
+
+        # TODO: calculate accuracies
